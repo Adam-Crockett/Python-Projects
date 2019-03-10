7bfb49aa6fccc9c180bb8ae8e9e982285c2c8246
Adam Crockett <arc1224@gmail.com>
2019-03-09 21:10:32 -0800

    Start on poor man translation

---
+++ PigLatin/poor_man_translator.py
@@ -0,0 +1,36 @@
+import pprint
+from collections import defaultdict
+
+
+def statement_request():
+    entry = input("Enter a phrase for us to deconstruct:\n").lower()
+    return entry
+
+
+def deconstruct(entry):
+    """
+    Function to create a deconstructed list of the statement letters
+    :param entry: The string input from user
+    :return: sorted list of letters used in statement
+    """
+    chart = defaultdict(list)
+    for letter in entry:
+        if letter.isalpha():
+            chart[letter].append(letter)
+    return sorted(chart.items())
+
+
+if __name__ == '__main__':
+    print("Hello, welcome to our statement analyzer. Input a statement and we will break it down into how many letters"
+          "it has.")
+    print("Enter 'stop' to quit.")
+
+    # Loop to keep asking for more statements
+    while True:
+        statement = statement_request()
+        if statement == "stop":
+            print("Thank you for using our services. Have a nice day.")
+            break
+        else:
+            pp = pprint.PrettyPrinter(width=110)
+            pp.pprint(deconstruct(statement))
